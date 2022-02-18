c350ca0bae5cc1ad9be94c33bf3076e65253e3a6
Jonathan George <jonathan@georges.co>
2022-02-18 00:07:41 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ function Home() {
   return (
     <Container>
       <Row className="mb-3">
-        <p>This is the home file.</p>
+        <p>This is a growing set of utilities to assist with daily operations. 
+           Functionality is currently in development, but the DCHP Option Helper and the Option 43 Generator are ready to go.
+           Both can be found under the "Utilities" menu above.</p>
+        <p>Please <a href="mailto:jgeorge@one-comm.com">submit</a> any bugs found.</p>
       </Row>
     </Container>
   );
