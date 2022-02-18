0f03d8ddcaac312341dd72d497fc40964e87c17e
Jonathan George <jonathan@georges.co>
2022-02-17 23:57:54 -0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {
 } from '@azure/msal-react';
 import Auth from './components/Global/Auth'
 import Header from './components/Global/Header';
+import Footer from './components/Global/Footer';
 import Home from './components/Home';
+import Projects from './components/Projects';
+import Project from './components/Project';
 import Option43 from './components/Utils/Option43';
 import Dhcp from './components/Utils/Dhcp';
-import ConnTest from './components/Utils/ConnTest';
+// import ConnTest from './components/Utils/ConnTest';
 import NotFound from './components/Global/NotFound';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -32,11 +35,14 @@ export default function App() {
           <GetHeader />
           <Routes>
             <Route exact path="/" element={ <Home /> } />
+            <Route exact path="/projects" element={ <Projects /> } />
+            <Route exact path="/project" element={ <Project /> } />
             <Route exact path="/utils/opt43" element={ <Option43 /> } />
             <Route exact path="/utils/dhcp" element={ <Dhcp /> } />
-            <Route exact path="/utils/conntest" element={ <ConnTest /> } />
+            {/* <Route exact path="/utils/conntest" element={ <ConnTest /> } /> */}
             <Route path="*" element={ <NotFound />} />
           </Routes>
+          <Footer />
         </Router>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
---
+++ src/components/Global/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer class="bg-light text-center text-lg-start fixed-bottom">
+      <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2);"}}>
+        © 2022 Copyright:&nbsp;
+        <a class="text-dark" href="https://one-comm.com/">One Communications</a><br />
+        Made with ♥️ in Arkansas.
+      </div>
+    </footer>
+  )
+}
