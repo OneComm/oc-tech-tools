29008ceebcf7f68491321e3e617dd6987e33747f
Jonathan George <jonathan@georges.co>
2022-08-27 12:57:23 -0500

    Update layout

--- src/components/Utils/Dhcp.js
+++ src/components/Utils/Dhcp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GenerateHex } from "../../api/dhcp";
-import { Row, Col, Form, FloatingLabel, Button, Collapse } from 'react-bootstrap';
+import { Row, Col, Form, FloatingLabel, Button, Collapse, Card } from 'react-bootstrap';
 
 function Dhcp() {
 
@@ -50,149 +50,154 @@ function Dhcp() {
 
   return (
     <div className="page-content p-3">
-      <Form onSubmit={ handleSubmit }>
-        <Row className="mb-3">
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingTFTP"
-              label="TFTP Server Address"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="sw_tftp" placeholder="TFTP Server Address" onChange={ handleChange } />
-            </FloatingLabel>
-            
-          </Form.Group>
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingCSRV"
-              label="Call Server Address"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="call_srv" placeholder="Call Server Address" onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-          <Form.Group as={ Col }>
-          <FloatingLabel
-              controlId="floatingVLAN"
-              label="VLAN ID"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="vlan" placeholder="VLAN ID" value={formData.vlan} onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-        <Form.Label>VLAN Priority (l2p)</Form.Label>
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingDefault"
-              label="default"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="l2p_default" placeholder="default" value={formData.l2p_default} onChange={ handleChange } />
-            </FloatingLabel>
-            
-          </Form.Group>
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingV"
-              label="voice media ( v )"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="l2p_voice" placeholder="voice" value={formData.l2p_voice} onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-          <Form.Group as={ Col }>
-          <FloatingLabel
-              controlId="floatingS"
-              label="voice signaling ( s )"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="l2p_signaling" placeholder="signaling" value={formData.l2p_signaling} onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-        <Form.Label>Diffserv Codepoint (dscp)</Form.Label>
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingDSCPDefault"
-              label="default"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="dscp_default" placeholder="default" value={formData.dscp_default} onChange={ handleChange } />
-            </FloatingLabel>
-            
-          </Form.Group>
-          <Form.Group as={ Col }>
-            <FloatingLabel
-              controlId="floatingDSCPV"
-              label="voice media ( v )"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="dscp_voice" placeholder="voice" value={formData.dscp_voice} onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-          <Form.Group as={ Col }>
-          <FloatingLabel
-              controlId="floatingDSCPS"
-              label="voice signaling ( s )"
-              className="mb-3"
-            >
-              <Form.Control type="text" name="dscp_signaling" placeholder="signaling" value={formData.dscp_signaling} onChange={ handleChange } />
-            </FloatingLabel>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Button bsPrefix='oc-btn-primary' onClick={ handleSubmit }>Generate</Button>
-        </Row>
-      </Form>
-      <Collapse in={ open }>
-        <div>
-          <Row className="mb-2">
-            <Col><b>Hex String</b></Col>
-            <Col>
-              <Form.Control as="textarea" style={{height: "130px"}} disabled value={ hexData.unformatted } />
-            </Col>
-            <Col>
-              <Button
-                bsPrefix='oc-btn-primary'
-                onClick={ () => navigator.clipboard.writeText(hexData.unformatted) }
-              >
-                Copy
-              </Button>
-            </Col>
-          </Row>
-          <Row className="mb-2">
-            <Col><b>Sophos Hex String</b></Col>
-            <Col>
-              <Form.Control as="textarea" style={{height: "160px"}} disabled value={ hexData.formatted } />
-            </Col>
-            <Col>
-              <Button
-                bsPrefix='oc-btn-primary'
-                onClick={ () => navigator.clipboard.writeText(hexData.formatted) }
-              >
-                Copy
-              </Button>
-            </Col>
-          </Row>
-          <Row className="mb-2">
-            <Col><b>ASCII String</b></Col>
-            <Col>
-              <Form.Control as="textarea" style={{height: "70px"}} disabled value={ hexData.ascii } />
-            </Col>
-            <Col>
-              <Button
-                bsPrefix='oc-btn-primary'
-                onClick={ () => navigator.clipboard.writeText(hexData.ascii) }
-              >
-                Copy
-              </Button>
-            </Col>
-          </Row>
-        </div>
-      </Collapse>
+      <Card>
+        <Card.Body>
+          <Card.Title className="border-bottom">DHCP Helper</Card.Title>
+          <Form className="py-2" onSubmit={ handleSubmit }>
+            <Row className="mb-3">
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingTFTP"
+                  label="TFTP Server Address"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="sw_tftp" placeholder="TFTP Server Address" onChange={ handleChange } />
+                </FloatingLabel>
+                
+              </Form.Group>
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingCSRV"
+                  label="Call Server Address"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="call_srv" placeholder="Call Server Address" onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group as={ Col }>
+              <FloatingLabel
+                  controlId="floatingVLAN"
+                  label="VLAN ID"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="vlan" placeholder="VLAN ID" value={formData.vlan} onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+            <Form.Label>VLAN Priority (l2p)</Form.Label>
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingDefault"
+                  label="default"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="l2p_default" placeholder="default" value={formData.l2p_default} onChange={ handleChange } />
+                </FloatingLabel>
+                
+              </Form.Group>
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingV"
+                  label="voice media ( v )"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="l2p_voice" placeholder="voice" value={formData.l2p_voice} onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group as={ Col }>
+              <FloatingLabel
+                  controlId="floatingS"
+                  label="voice signaling ( s )"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="l2p_signaling" placeholder="signaling" value={formData.l2p_signaling} onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+            <Form.Label>Diffserv Codepoint (dscp)</Form.Label>
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingDSCPDefault"
+                  label="default"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="dscp_default" placeholder="default" value={formData.dscp_default} onChange={ handleChange } />
+                </FloatingLabel>
+                
+              </Form.Group>
+              <Form.Group as={ Col }>
+                <FloatingLabel
+                  controlId="floatingDSCPV"
+                  label="voice media ( v )"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="dscp_voice" placeholder="voice" value={formData.dscp_voice} onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group as={ Col }>
+              <FloatingLabel
+                  controlId="floatingDSCPS"
+                  label="voice signaling ( s )"
+                  className="mb-3"
+                >
+                  <Form.Control type="text" name="dscp_signaling" placeholder="signaling" value={formData.dscp_signaling} onChange={ handleChange } />
+                </FloatingLabel>
+              </Form.Group>
+            </Row>
+            <Row className="m-4">
+              <Button bsPrefix='oc-btn-primary' onClick={ handleSubmit }>Generate</Button>
+            </Row>
+          </Form>
+          <Collapse in={ open }>
+            <div>
+              <Row className="mb-2">
+                <Col><b>Hex String</b></Col>
+                <Col>
+                  <Form.Control as="textarea" style={{height: "130px"}} disabled value={ hexData.unformatted } />
+                </Col>
+                <Col>
+                  <Button
+                    bsPrefix='oc-btn-primary'
+                    onClick={ () => navigator.clipboard.writeText(hexData.unformatted) }
+                  >
+                    Copy
+                  </Button>
+                </Col>
+              </Row>
+              <Row className="mb-2">
+                <Col><b>Sophos Hex String</b></Col>
+                <Col>
+                  <Form.Control as="textarea" style={{height: "160px"}} disabled value={ hexData.formatted } />
+                </Col>
+                <Col>
+                  <Button
+                    bsPrefix='oc-btn-primary'
+                    onClick={ () => navigator.clipboard.writeText(hexData.formatted) }
+                  >
+                    Copy
+                  </Button>
+                </Col>
+              </Row>
+              <Row className="mb-2">
+                <Col><b>ASCII String</b></Col>
+                <Col>
+                  <Form.Control as="textarea" style={{height: "70px"}} disabled value={ hexData.ascii } />
+                </Col>
+                <Col>
+                  <Button
+                    bsPrefix='oc-btn-primary'
+                    onClick={ () => navigator.clipboard.writeText(hexData.ascii) }
+                  >
+                    Copy
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          </Collapse>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
