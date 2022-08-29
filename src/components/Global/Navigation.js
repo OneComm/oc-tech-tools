7bafaae34c43f914b10f9bd4307084607af83d2d
Jonathan George <jonathan@georges.co>
2022-08-29 16:58:21 -0500

    Add link to brand

--- src/components/Global/Navigation.js
+++ src/components/Global/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../../assets/img/logo.svg';
 import ocLogo from '../../assets/img/oc-logo.png';
@@ -9,19 +9,33 @@ function Navigation(functionLoading) {
   return (
     <div className='d-flex flex-column vertical-nav text-white' id='sidebar'>
       <div className="py-4 px-3 mb-4">
-        <div className="media d-flex align-items-center">
-          <img src={logo} alt="" width="70" className="d-inline-block align-top" />{' '}
-          <div className="media-body">
-            <h4 className="m-0">Tech Tools</h4>
+        <LinkContainer to="/" style={{cursor: 'pointer'}}>
+          <div className="media d-flex align-items-center">
+            <img src={logo} alt="" width="70" className="d-inline-block align-top" />{' '}
+            <div className="media-body">
+              <h4 className="m-0">Tech Tools</h4>
+            </div>
           </div>
-        </div>
+        </LinkContainer>
         <hr />
         <div className='p-4'>
           <Navbar variant='dark'>
             <Nav className="flex-column">
               <Navbar.Text as='h6' className='text-uppercase text-white'>Applications</Navbar.Text>
               <LinkContainer to="/"><Nav.Link>Dashboard</Nav.Link></LinkContainer>
-              <LinkContainer to="/sow"><Nav.Link>Scope of Work</Nav.Link></LinkContainer>
+              <LinkContainer to="/sow">
+                <OverlayTrigger
+                  key='sow'
+                  placement='right'
+                  overlay={
+                    <Tooltip id={`tooltip-right`}>
+                      Scope of Work Tool
+                    </Tooltip>
+                  }
+                >
+                  <Nav.Link>SOW</Nav.Link>
+                </OverlayTrigger>
+              </LinkContainer>
               <hr/>
               <Navbar.Text as='h6' className='text-uppercase text-white'>Utilities</Navbar.Text>
               <LinkContainer to="/utils/opt43"><Nav.Link>Option 43 Generator</Nav.Link></LinkContainer>
