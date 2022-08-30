90614fc4f94fef6d4175f18b802adb047527b355
Jonathan George <jonathan@georges.co>
2022-08-30 11:45:57 -0500

    Fix logo

--- src/components/Global/Navigation.js
+++ src/components/Global/Navigation.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import { Nav, Navbar, OverlayTrigger,Tooltip } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.svg';
 import ocLogo from '../../assets/img/oc-logo.png';
 import project from '../../../package.json';
-import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <div className='d-flex flex-column vertical-nav text-white' id='sidebar'>
       <div className="py-4 px-3 mb-4">
-        <LinkContainer to="/" style={{cursor: 'pointer'}}>
+        <Link to="/" style={{cursor: 'pointer', textDecoration: 'none', color: 'white'}}>
           <div className="media d-flex align-items-center">
             <img src={logo} alt="" width="70" className="d-inline-block align-top" />{' '}
             <div className="media-body">
               <h4 className="m-0">Tech Tools</h4>
             </div>
           </div>
-        </LinkContainer>
+        </Link>
         <hr />
         <div className='p-4'>
           <Navbar variant='dark'>
             <Nav className="flex-column">
               <Navbar.Text as='h6' className='text-uppercase text-white'>Applications</Navbar.Text>
               <Link className='nav-link' to="/">Dashboard</Link>
+              <Link className='nav-link' to="/sow">
+                <OverlayTrigger
+                  key='sow'
+                  placement='right'
+                  overlay={
+                    <Tooltip>
+                      Tool for generating and working project scopes
+                    </Tooltip>
+                  }
+                >
+                  <p>SOW</p>
+                </OverlayTrigger>
+              </Link>
               <hr/>
               <Navbar.Text as='h6' className='text-uppercase text-white'>Utilities</Navbar.Text>
               <Link className='nav-link' to="/utils/opt43">
