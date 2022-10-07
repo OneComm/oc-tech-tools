da647e4da7f9416bc8de03ab3580adf04e542ca8
Jonathan George <jonathan@georges.co>
2022-10-07 12:30:02 -0500

    move headers axios to options

--- src/api/whoisxmlapi.js
+++ src/api/whoisxmlapi.js
@@ -5,16 +5,13 @@ export async function LookupDNS(domain) {
   const domainName = domain;
 
   const api = axios.create({
-    baseURL: "https://www.whoisxmlapi.com/whoisserver"
-  });
-
-  const options = {
+    baseURL: "https://www.whoisxmlapi.com/whoisserver",
     Headers: {
       "Content-Type": "text/json"
     }
-  }
+  });
 
-  api.get(`/DNSService?apiKey=${apiKey}&domainName=${domainName}&type=_all&outputFormat=json`, options)
+  api.get(`/DNSService?apiKey=${apiKey}&domainName=${domainName}&type=_all&outputFormat=json`)
     .then(response => {
       console.log(response.data);
       return response.data;
