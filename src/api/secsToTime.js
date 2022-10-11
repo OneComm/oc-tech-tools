5d98ab447b979654bfdfa0bf91a6bc74ff04de7d
Jonathan George <jonathan@georges.co>
2022-10-11 16:08:28 -0500

    fix comparison

--- src/api/secsToTime.js
+++ src/api/secsToTime.js
@@ -3,7 +3,7 @@ export default function SecsToTime(d) {
   var h = Math.floor(d / 3600);
   var m = Math.floor(d % 3600 / 60);
 
-  var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+  var hDisplay = h > 0 ? h + (h === 1 ? " hour " : " hours ") : "";
+  var mDisplay = m > 0 ? m + (m === 1 ? " minute " : " minutes ") : "";
   return hDisplay + mDisplay; 
 }
