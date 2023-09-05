e95d202c99bc53b6f7dc8d2d6e71d3d597b537a9
Jonathan George <jonathan@georges.co>
2023-09-05 11:01:38 -0500

    update whoisapi for cors

--- src/api/whoisxmlapi.ts
+++ src/api/whoisxmlapi.ts
@@ -1,46 +1,47 @@
 import axios from "axios";
 
-export async function LookupDomain(domain) {
+export async function LookupDomain(domain: string) {
+  const baseURL = "https://oc-tech-tools-api.herokuapp.com";
   const apiKey = process.env.REACT_APP_WHOIS_API_KEY;
   const domainName = domain;
 
   const whoisApi = axios.create({
-    baseURL: "https://www.whoisxmlapi.com/whoisserver",
-    headers: {
-      "Content-Type": "text/json",
-      "Referer": "https://tech-tools.one-comm.com",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-    },
-    withCredentials: true, // Include cookies in the request
+    baseURL,
   });
 
   try {
-    const res = await whoisApi.get(`/WhoisService?apiKey=${apiKey}&domainName=${domainName}&outputFormat=json`);
-    console.log(res);
-    return res;
+    const domain = await whoisApi.get('/whois/whois', {
+      params: {
+        domainName,
+        apiKey,
+      }
+    });
+    console.log(domain);
+    return domain;
   } catch (error) {
     
   }
 }
 
-export async function LookupDNS(domain) {
+export async function LookupDNS(domain: string) {
+  const baseURL = "https://oc-tech-tools-api.herokuapp.com";
   const apiKey = process.env.REACT_APP_WHOIS_API_KEY;
   const domainName = domain;
 
   const whoisApi = axios.create({
-    baseURL: "https://www.whoisxmlapi.com/whoisserver",
-    headers: {
-      "Content-Type": "text/json",
-      "Referer": "https://tech-tools.one-comm.com",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-    },
-    withCredentials: true, // Include cookies in the request
+    baseURL,
   });
 
   try {
-    const res = await whoisApi.get(`/DNSService?apiKey=${apiKey}&domainName=${domainName}&type=A,MX,TXT,AAAA,&outputFormat=json`);
-    return res;
+    const dns = await whoisApi.get('/whois/dns', {
+      params: {
+        domainName,
+        apiKey,
+      }
+    });
+    console.log(dns);
+    return dns;
   } catch (error) {
-    console.log(error);
+    
   }
 }
