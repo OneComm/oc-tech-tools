28cb389c5549123932d9799caef6457bb74c0016
Jonathan George <jonathan@georges.co>
2023-09-08 13:49:48 -0500

    fix loading display

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -3,12 +3,10 @@ import Spinner from 'react-bootstrap/Spinner';
 
 export default function Loading() {
   return (
-    <div className='page-content vertical-center'>
-      <center>
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </center>
+    <div className='page-content vertical-center' style={{position: 'fixed', top: '50%', left: '40%'}}>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     </div>
   );
 }
